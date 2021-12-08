7283ac9428f9f59e7fe776de0eb6f8a8e8ed9200
Marcel <mibes@avaya.com>
2021-12-08 08:34:56 +0100

    CLI

---
+++ README.md
@@ -0,0 +1,7 @@
+# Installation
+
+This library depends on the "dot" binary being installed. It comes as part of the ["graphviz"](https://graphviz.org/download/) 
+set of tools.
+
+On a Mac you can install this with `brew install graphviz`. See the [graphviz page](https://graphviz.org/download/) for 
+installation instructions for other operating systems.
--- yuml-cli/src/main.rs
+++ yuml-cli/src/main.rs
@@ -1,17 +1,38 @@
+use std::fs::read_to_string;
+
+use clap::{App, Arg, SubCommand};
 use yuml_rs::{process_yuml_document, render_svg_from_dot};
 
-fn main() {
-    let text = r#"
-    // {type:activity}
-    // {generate:true}
+const SAMPLE_YUML: &str = r#"
+// {type:activity}
+// {generate:true}
 
-    (start)-><a>[kettle empty]->(Fill Kettle)->|b|
-    <a>[kettle full]->|b|->(Boil Kettle)->|c|
-    |b|->(Add Tea Bag)->(Add Milk)->|c|->(Pour Water)
-    (Pour Water)->(end)
+(start)-><a>[kettle empty]->(Fill Kettle)->|b|
+<a>[kettle full]->|b|->(Boil Kettle)->|c|
+|b|->(Add Tea Bag)->(Add Milk)->|c|->(Pour Water)
+(Pour Water)->(end)
 "#;
 
-    let dot = match process_yuml_document(text, false) {
+fn main() {
+    let matches = App::new("yUML diagram utility")
+        .version("0.1")
+        .author("Marcel Ibes <mibes@avaya.com>")
+        .about("Renders SVG and PNG images based on yUML input")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("Sets the input yUML file")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let input_file = matches.value_of("input").expect("an input file is mandatory");
+    let text = read_to_string(input_file).expect("can not read input file");
+
+    let dot = match process_yuml_document(&text, false) {
         Ok(dot) => dot,
         Err(err) => {
             println!("{}", err);
