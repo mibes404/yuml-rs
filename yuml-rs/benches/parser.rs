284157db4e690075e9efaf8936609ff2bdd7eb62
Marcel <mibes@avaya.com>
2021-12-17 16:47:47 +0100

    Updated benchmarks for class diagrams

--- yuml-rs/benches/parser.rs
+++ yuml-rs/benches/parser.rs
@@ -4,6 +4,9 @@ use yuml_rs::parse_yuml;
 pub fn criterion_benchmark(c: &mut Criterion) {
     let yuml = include_bytes!("../test/activity.yuml");
     c.bench_function("activity.yuml", |b| b.iter(|| parse_yuml(yuml)));
+
+    let yuml = include_bytes!("../test/class.yuml");
+    c.bench_function("class.yuml", |b| b.iter(|| parse_yuml(yuml)));
 }
 
 criterion_group!(benches, criterion_benchmark);
